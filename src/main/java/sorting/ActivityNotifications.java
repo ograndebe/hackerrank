6543f5ddefb0f194855576798c2125ac6dd74db3
ograndebe <ograndebe@gmail.com>
2020-03-10 22:38:25 -0300

    Solution with understanding

--- src/main/java/sorting/ActivityNotifications.java
+++ src/main/java/sorting/ActivityNotifications.java
@@ -2,6 +2,7 @@ package sorting;
 
 import hashtables.FreqQuery;
 import org.junit.jupiter.api.Test;
+import util.MyUtils;
 
 import java.io.IOException;
 import java.util.*;
@@ -13,132 +14,48 @@ public class ActivityNotifications {
 
     static int activityNotifications(int[] expenditure, int d) {
 
-        int auxArray[] = new int[201];
+        int counterArray[] = new int[201];
 
         for(int i = 0; i < d; i++){
-            auxArray[expenditure[i]]++;
+            counterArray[expenditure[i]]++;
         }
 
         int middleIndex = d/2;
         boolean odd = d%2!= 0;
+        if (odd) {
+            middleIndex += 1;
+        }
         int alerts = 0;
         LinkedList<Integer> l = new LinkedList<>();
         for (int index = d; index < expenditure.length ; index++) {
-            double median  =  calcMedian(auxArray, middleIndex,  odd);
+            double median  =  calcMedian(counterArray, d,  middleIndex,  odd);
             int expense = expenditure[index];
             if (expense >= 2*median) {
                 alerts++;
-//                    System.out.printf(" **%s",alerts);
                 l.add(index);
             }
 
-            auxArray[expenditure[index]]++;
-            auxArray[expenditure[index - d]]--;
-        }
-        System.out.println(l);
-        return alerts;
-
-    }
-
-    public static double calcMedian(int[] auxArray, int middleIndex, boolean odd){
-        //recreating until the middle
-//        int orderedUntilTheMiddle[] = new int[middleIndex+1];
-        int newIndex = 0;
-        int last = -1;
-        for(int i = 0; i < auxArray.length; i++){
-            int elementCounting = auxArray[i];
-            if (elementCounting > 0) {
-                newIndex += elementCounting;
-                if (newIndex >= middleIndex) {
-                    if (newIndex == middleIndex) {
-                        return odd || elementCounting > 1?i:(last+i)/2D;
-                    } else {
-                        return i;
-                    }
-                }
-                last = i;
-            }
-        }
-
-        return -1;
-    }
-
-    void sort(int[] arr, int max) {
-        int[] count = new int[max+1];
-
-        for (int i = 0; i < arr.length; i++) count[arr[i]]++;
-
-        for (int i = 1; i <= max; ++i)
-            count[i] += count[i - 1];
-
-        int[] output = new int[arr.length + 1];
-
-        for (int i = arr.length - 1; i >= 0; i--) {
-            output[count[arr[i]] - 1] = arr[i];
-            count[arr[i]]--;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = output[i];
-        }
-    }
-
-
-    static int activityNotificationsBest(int[] expenditure, int d) {
-
-        int auxArray[] = new int[201];
-
-        for(int i = 0; i < d; i++){
-            auxArray[expenditure[i]]++;
-        }
-
-        int middleIndex = d/2;
-        boolean odd = d%2!= 0;
-        int alerts = 0;
-        for (int index = d; index < expenditure.length ; index++) {
-            double median  =  calcMedian(auxArray, middleIndex,  odd);
-            int expense = expenditure[index ];
-            if (expense >= 2*median) {
-                alerts++;
-//                    System.out.printf(" **%s",alerts);
-            }
-
-            auxArray[expenditure[index]]++;
-            auxArray[expenditure[index - d]]--;
+            counterArray[expenditure[index]]++;
+            counterArray[expenditure[index - d]]--;
         }
         return alerts;
 
     }
 
-    public static double calcMedianBest(int[] auxArray, int middleIndex, boolean odd){
-        //recreating until the middle
-        int orderedUntilTheMiddle[] = new int[middleIndex+1];
-        int newIndex = 0;
-        for(int i = 0; i < auxArray.length; i++){
-            int elementCounting = auxArray[i];
-            if (elementCounting > 0) {
-                newIndex += elementCounting;
-                if (newIndex >= middleIndex) {
-                    newIndex-= elementCounting;
-                    for (int x = 0; x < elementCounting ; x++) {
-                        orderedUntilTheMiddle[newIndex++] = i;
-                        if (newIndex > middleIndex) {
-                            x = elementCounting;
-                            i = auxArray.length;
-                        }
-                    }
-
-                }
-            }
-        }
-//        System.out.printf("%s -> %s\n", middleIndex, orderedUntilTheMiddle[middleIndex]);
-        double median;
-        if (odd) {
-            median = orderedUntilTheMiddle[middleIndex];
-        } else {
-            median = (orderedUntilTheMiddle[middleIndex-1]+orderedUntilTheMiddle[middleIndex])/2D;
+    public static double calcMedian(int[] counterArray, int d, int middleIndex, boolean odd) {
+        int counter = 0, left = 0;
+        while (counter < middleIndex) {
+            counter += counterArray[left++];
         }
 
-        return median;
+        int right = left;
+        left -= 1;
+
+        if (counter > middleIndex || odd) return left;
+        else {
+            while (counterArray[right] == 0) right ++;
+            return (left + right)/2d;
+        }
     }
 
 
